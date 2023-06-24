25d8a0c2d4026848d93ce079efafb21bffdcdf0d
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2023-06-24 15:22:06 +0300

    Müşteri Tabloları ilişkilendirildi.

---
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20230624113338_deneme1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyCashIdentityProject.DataAccessLayer.Migrations
+{
+    public partial class deneme1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerAccountNumber",
+                table: "CustomerAccounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerAccountCurrency",
+                table: "CustomerAccounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankBranch",
+                table: "CustomerAccounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerAccountNumber",
+                table: "CustomerAccounts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerAccountCurrency",
+                table: "CustomerAccounts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankBranch",
+                table: "CustomerAccounts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
@@ -8,12 +8,15 @@ namespace EasyCashIdentityProject.EntityLayer.Concrete
 {
     public class CustomerAccount
     {
+
         public int CustomerAccountID { get; set; }
-        public string? CustomerAccountNumber { get; set; }
-        public string? CustomerAccountCurrency { get; set; }
+        public string CustomerAccountNumber { get; set; }
+        public string CustomerAccountCurrency { get; set; }
         public decimal CustomerAccountBalance { get; set; }
-        public string? BankBranch { get; set; }
+        public string BankBranch { get; set; }
         public int AppUserID { get; set; }
         public AppUser AppUser { get; set; }
+        public List<CustomerAccountProcess> CustomerSender { get; set; }
+        public List<CustomerAccountProcess> CustomerReceiver { get; set; }
     }
 }
